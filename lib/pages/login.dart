78f583410d5f68254d0165f856b2fbf1420b2ed2
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-12-03 21:12:07 -0600

    adding logos to home screen

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,18 +1,60 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-
-
-
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  List<dynamic> socialMedias = [
+    'assets/google.png',
+    'assets/facebook.png',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal:30,
+          vertical:50
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+            Text('Inicia sesión!',
+              style: TextStyle(
+                fontSize: 40
+              ),
+            ),
+            Text('Escoje una opción para loguearte',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w100
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              child: Wrap(
+                spacing: 50,
+                children: socialMedias.asMap().map((int index, dynamic e){ 
+                return MapEntry(index, 
+                    //Text('${socialMedias[index]}')
+                    Image(
+                    height: 60,
+                    width: 60,
+                    image: AssetImage(socialMedias[index]),
+                    )
+                  );
+                }).values.toList(),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
